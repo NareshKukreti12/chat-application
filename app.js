27b2bba056a29e2f75919311dab4a615f87c80f8
NareshKukreti12 <kukreti12.naresh@gmail.com>
2019-01-31 09:56:58 +0530

    31 January

--- app.js
+++ app.js
@@ -46,19 +46,27 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   io.on('connection',(socket)=>{
     console.log('New user connected')
 
-   
+    socket.emit('newMessage',{
+      from:'Admin',
+      text:'Welcome to the chat app'
+    })
+    socket.broadcast.emit('newMessage',{
+      from:'Admin',
+      text:'New user joined'
+    })
+
     socket.on('createMessage',(message)=>{
         console.log('CreateMessage',message);
-        // io.emit('newMessage',{
-        //   from:message.from,
-        //   text:message.text,
-        //   createAt:new Date().getTime()
-        // })
-         socket.broadcast.emit('newMessage',{
-           from:message.from,
-           text:message.text,
-           createAt: new Date().getTime()
-         });
+        io.emit('newMessage',{
+          from:message.from,
+          text:message.text,
+          createAt:new Date().getTime()
+        })
+        //  socket.broadcast.emit('newMessage',{
+        //    from:message.from,
+        //    text:message.text,
+        //    createAt: new Date().getTime()
+        //  });
     })
 
     socket.on('disconnect',()=>{
